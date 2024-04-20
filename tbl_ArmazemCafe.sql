5bb3d35fa7a0987c73be0850d374503d2fd5eb77
ryangsantos2 <ryan.santos@sptech.school>
2024-04-19 22:46:54 -0300

    Entidades alteradas e configuradas mediante as modelagens lógicas

--- tbl_ArmazemCafe.sql
+++ tbl_ArmazemCafe.sql
@@ -1,74 +1,100 @@
+CREATE DATABASE aluno;
 USE aluno;
 
 
-
+-- Tabela que armazenara a temperatura/umidade minima e maxima, que alertara o usuario caso haja mudanças fora das indicações
+CREATE TABLE alerta (
+  idAlerta INT PRIMARY KEY AUTO_INCREMENT,
+  umidade_max DECIMAL(5,2) NULL,
+  temperatura_max DECIMAL(5,2) NULL,
+  umidade_min DECIMAL(5,2) NULL,
+  temperatura_min DECIMAL(5,2) NULL);
+  
+  
 -- UMA TABELA PARA AS EMPRESAS QUE CONTRATAREM OS NOSSOS SERVIÇOS
 CREATE TABLE empresa (
-    idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
-    cnpj VARCHAR(20) UNIQUE NOT NULL,
-    nome VARCHAR(255) NOT NULL,
-    categoria VARCHAR(255),
-    endereco VARCHAR(255),
-    contato VARCHAR(255),
-    representante VARCHAR(255)
-);
+  idEmpresa INT,
+  cnpj CHAR(14) NOT NULL,
+  nome VARCHAR(255) NOT NULL,
+  categoria VARCHAR(70),
+  cep CHAR(9) NOT NULl,
+  numero VARCHAR(45),
+  complemento VARCHAR(60),
+  telCelular VARCHAR(11),
+  telFixo VARCHAR(10),
+  representante VARCHAR(255)
+  );
+  
 
 
 -- Tabela para os funcionarios fazerem login no site para acessar os gráficos
 CREATE TABLE funcionario (
-    idFuncionario INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE,
-    cargo VARCHAR(255) NOT NULL,
-    senha VARCHAR(255),
-    fkEmpresa INT,
-      CONSTRAINT fkEmpresaFuncionario FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa),
-    fkSupervisor INT,
-	 CONSTRAINT fkSupervisorFuncionario FOREIGN KEY (fkSupervisor) REFERENCES funcionario(idFuncionario)
+idFuncionario INT,
+fkEmpresa INT,
+	PRIMARY KEY (idFuncionario, fkEmpresa),
+nome VARCHAR(255) NOT NULL,
+email VARCHAR(256) UNIQUE,
+	CONSTRAINT chkEmail
+		CHECK (email LIKE('%@%', '%.%')),
+cargo VARCHAR(255) NOT NULL,
+senha VARCHAR(255) ,
+fkSupervisor INT ,
+  CONSTRAINT supervisor_funcionario
+		FOREIGN KEY (fkSupervisor)
+			REFERENCES funcionario (idFuncionario),
+            
+  CONSTRAINT fk_Funcionario_Empresa1
+		FOREIGN KEY (fkEmpresa)
+			REFERENCES Empresa (idEmpresa)
 );
+
+ 
+  
 
 -- Tabela para manter registro da localização de cada armazem 
-CREATE TABLE armazem (
-    idArmazem INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(255) NOT NULL,
-    localizacao VARCHAR(255),
-    capacidade_toneladas INT
+CREATE TABLE armazem(
+  idArmazem INT PRIMARY KEY AUTO_INCREMENT,
+  nome VARCHAR(255) NOT NULL,
+  localizacao VARCHAR(255),
+  capacidade_toneladas INT,
+  fkEmpresa INT , 
+	CONSTRAINT fk_Armazem_Empresa1
+		FOREIGN KEY (fkEmpresa)
+			REFERENCES empresa (idEmpresa),
+  fkAlerta INT ,
+	CONSTRAINT alerta
+		FOREIGN KEY (fkAlerta)
+			REFERENCES alerta(idAlerta)
 );
-
+   
 
 -- Tabela de Dispositivos para sabermos a localização de cada um deles e entregar um melhor monitoramento, já que se por exemplo o alerta aparecer que algum local está com a temp ou 
 -- umidade abaixo do recomendado, vai ser mais fácil localizar se você souber o local onde está ocorrendo isso
 CREATE TABLE dispositivo_monitoramento (
-    idDispositivo INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL,
-    localizacao VARCHAR(255),
-    fkArmazem INT,
-		CONSTRAINT fkDispositivoArmazem FOREIGN KEY (fkArmazem) REFERENCES armazem(idArmazem)
-);
-
+  idDispositivo INT PRIMARY KEY,
+  nome VARCHAR(255),
+  localizacao VARCHAR(255) NULL,
+  fkArmazem INT,
+  CONSTRAINT dispositivo_armazem
+    FOREIGN KEY (fkArmazem)
+    REFERENCES amazem (idArmazem)
+    );
+    
 -- Tabela pra receber os dados capturados do arduino
-CREATE TABLE dados_monitoramento (
-    idDados INT PRIMARY KEY AUTO_INCREMENT,
-	data_hora DATETIME,
-    temperatura DECIMAL(5,2),
-    umidade DECIMAL(5,2),
-    fkDispositivo INT,
-    CONSTRAINT fkDadosDispositivo FOREIGN KEY (fkDispositivo) REFERENCES dispositivo_monitoramento(idDispositivo)
-);
-
--- Tabela para monitorar quem entrou no sistema, a data e hora e o que a pessoa fez
-CREATE TABLE registro_sistema (
-    idRegistro INT PRIMARY KEY AUTO_INCREMENT,
-    data_hora DATETIME,
-    acao VARCHAR(255),
-    fkFuncionario INT,
-    CONSTRAINT fkRegistroFuncionario FOREIGN KEY (fkFuncionario) REFERENCES funcionario(idFuncionario)
-);
-
--- só um check para os funcionarios
-ALTER TABLE funcionario
-ADD CONSTRAINT Check_Cargo CHECK (cargo IN ('Gerente', 'Supervisor', 'Funcionário'));
-
+CREATE TABLE dados_monitoramento(
+  idDados INt,  
+  fkDispositivo INT,
+	PRIMARY KEY (idDados, fkDispositivo),
+  data_horaCaptura DATETIME,
+  temperatura DECIMAL(5,2),
+  umidade DECIMAL(5,2),
+	CONSTRAINT dados_dispositivo
+		FOREIGN KEY (fkDispositivo)
+			REFERENCES dispositivo_monitoramento (idDispositivo)
+    );
+  
+  
+-- Inserts das respectivas entidades
 
 INSERT INTO empresa (cnpj, nome, categoria, endereco, contato, representante) VALUES
 ('123456789', 'Empresa Mamacitas no poder do café', 'Alimentos', 'Rua Mimimi, 123', 'mamacitas@empresa.com', 'Laura Seda'),
@@ -81,6 +107,7 @@ INSERT INTO armazem (nome, localizacao, capacidade_toneladas) VALUES
 INSERT INTO dispositivo_monitoramento (nome, localizacao, fkArmazem) VALUES
 ('Sensor 1', 'Armazém A', 1),
 ('Sensor 2', 'Armazém B', 2);
+
 
 INSERT INTO funcionario (nome, email, cargo, senha, fkEmpresa, fkSupervisor) VALUES
 ('Patrick Bateman', 'Bateman@americano.com', 'Gerente', 'paulallen', 1, NULL),
